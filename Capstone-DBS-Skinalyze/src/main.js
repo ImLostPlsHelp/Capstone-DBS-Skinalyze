c0add343e6d0420aae1929386582126ed204a361
ImLostPlsHelp <107686662+ImLostPlsHelp@users.noreply.github.com>
2025-05-19 14:57:39 +0700

    Added navbar.html

--- Capstone-DBS-Skinalyze/src/main.js
+++ Capstone-DBS-Skinalyze/src/main.js
@@ -1,26 +1,12 @@
-import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.js";
+async function loadComponent(id, path) {
+  const res = await fetch(path);
+  const html = await res.text();
+  document.getElementById(id).innerHTML = html;
+}
 
-document.querySelector("#app").innerHTML = `
-  <div class="relative top-0 bg-cover bg-center opcaity-70" style="background-image: url('../public/skin-check.png')">
-    <div class="absolute top-0 right-0 w-48 h-48 bg-teal-300 rounded-full opacity-70 z-0"></div>
-    <div class="absolute bottom-0 right-0 w-48 h-48 bg-teal-300 rounded-full opacity-70 z-0"></div>
-
-    <div class="flex items-center justify-center min-h-screen">
-      <div class="text-center max-w-xl px-4 z-10">
-        <h1 class="text-6xl font-bold text-blue-900 mb-6">Skinalyze</h1>
-        <p class="text-gray-600 text-base md:text-lg leading-relaxed mb-6">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat.
-        </p>
-        <button class="bg-blue-900 text-white px-6 py-3 rounded-md hover:bg-blue-700 transition-all text-sm">
-          Check Now
-        </button>
-      </div>
-    </div>
-  </div>
-`;
+loadComponent('navbar', '/src/component/navbar.html');
+// loadComponent('hero', '/src/components/hero.html');
+// loadComponent('tentang-kami', '/src/components/tentang-kami.html');
+// loadComponent('artikel', '/src/components/artikel.html');
+// loadComponent('faq', '/src/components/faq.html');
+// loadComponent('footer', '/src/components/footer.html');
