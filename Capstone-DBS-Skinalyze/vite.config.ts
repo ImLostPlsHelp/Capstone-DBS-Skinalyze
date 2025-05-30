373e6a5d40298a27a2c9f13dee932bd014bc53b1
ImLostPlsHelp <maurits1005@gmail.com>
2025-05-30 14:01:41 +0700

    First attempt at check skin page and routing

--- Capstone-DBS-Skinalyze/vite.config.ts
+++ Capstone-DBS-Skinalyze/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        skin_check: resolve(__dirname, 'src/pages/skin-check.html'),
+      }
+    }
+  }
 })
