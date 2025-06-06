774d503db9a26367eb43f42d2a64fc715e37bad9
ImLostPlsHelp <maurits1005@gmail.com>
2025-06-06 16:00:20 +0700

    Fixing directory and rollupoptions

--- Capstone-DBS-Skinalyze/vite.config.ts
+++ Capstone-DBS-Skinalyze/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
+import { profile } from 'node:console'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
@@ -17,6 +18,10 @@ export default defineConfig({
         artikel: resolve(__dirname, 'artikel.html'),
         faq: resolve(__dirname, 'faq.html'),
         hasil: resolve(__dirname, 'hasil.html'),
+        login: resolve(__dirname, 'login.html'),
+        register: resolve(__dirname, 'register.html'),
+        profile: resolve(__dirname, 'profile.html'),
+        about: resolve(__dirname, 'about.html'),
       }
     }
   }
